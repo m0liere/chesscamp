1ff5cab6a5fedb8d3c218c2618f8d3cd821037d6
Roei <rmc@andrew.cmu.edu>
2014-04-16 15:29:28 -0400

    wrote relationships and some requird validations and respective tests in student model/test

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,18 @@ class Student < ActiveRecord::Base
 	has_many(:registrations)
 	belongs_to(:family)
 
-	
+	#validations
+	#must have family_id, first_name, last_name
+	validates_presence_of(:family_id)
+	validates_presence_of(:first_name)
+	validates_presence_of(:last_name)
+
+	#numerical vaidations
+	ratings_array = [0] + (100..3000).to_a
+	validates :rating, numericality: {only_integer: true}, inclusion: {in: ratings_array}
+
+
+	#custom validiatons
+	#student must belong to a family that is ACTIVE in the system
 
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -4,4 +4,24 @@ class StudentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  #test relationships
+  should have_many(:registrations)
+  should belong_to(:family)
+
+  #test validations
+  should validate_presence_of(:family_id)
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
+
+  #rating in correct range
+  should allow_value(0).for(:rating)
+  should allow_value(3000).for(:rating)
+  should allow_value(100).for(:rating)
+  should allow_value(420).for(:rating)
+
+  should_not allow_value(-1).for(:rating)
+  should_not allow_value(50).for(:rating)
+  should_not allow_value(5000).for(:rating)
+
 end
