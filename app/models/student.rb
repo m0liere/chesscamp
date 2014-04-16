ea1a64de9994da2b686b513b35f1a33ea9cbb2a1
Roei <rmc@andrew.cmu.edu>
2014-04-16 18:36:50 -0400

    finished some scopes and additional functions for student model, started to assemble family model

--- app/models/student.rb
+++ app/models/student.rb
@@ -11,10 +11,42 @@ class Student < ActiveRecord::Base
 
 	#numerical vaidations
 	ratings_array = [0] + (100..3000).to_a
-	validates :rating, numericality: {only_integer: true}, inclusion: {in: ratings_array}
+	validates :rating, numericality: {only_integer: true}, inclusion: {in: ratings_array}, presence: true
+
+	validates_date :date_of_birth
+	validates_numericality_of(:family_id)
+
+	#scopes
+	scope :alphabetical, -> order('last_name, first_name')
+	scope :active, -> where(active: true)
+	scope :inactive, -> where(active: false)
+	#all students below given rating lvl
+	scope :below_rating, -> (rating) {where('rating < ?', rating)}
+	scope :at_or_above_rating, -> (rating) {where('rating >= ?',rating)}
 
 
-	#custom validiatons
-	#student must belong to a family that is ACTIVE in the system
+	#additonal functions
+	
+	#function to run before the save in the data base to set unrated players to 0
+	def set_unrated
+		if(self.rating.nil?)
+			self.rating = 0;
+		end
+	end
 
+	#name
+	#propername
+	#age
+
+	def name
+    last_name + ", " + first_name
+  end
+  
+  def proper_name
+    first_name + " " + last_name
+  end
+
+	def age
+
+	end
 end
